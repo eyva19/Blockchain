4a1e79088a839451e82da0411557098278564a8b
Eyva <YevheniyaPo@gmail.com>
2023-10-22 15:16:00 -0700

    Updates

--- pychain.py
+++ pychain.py
@@ -72,7 +72,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -193,7 +193,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=input_data,
         creator_id=42,
         prev_hash=prev_block_hash
     )
